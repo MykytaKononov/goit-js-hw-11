59274af93140f8c8d22d7feee440da0ef337424b
MykytaKononov <nikitamaxkononov@gmail.com>
2025-02-01 13:44:06 +0100

    fix

--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { searchImages } from './js/pixabay-api.js';
 import { displayImages } from './js/render-function.js';
 
-document.getElementById('searchbutton').addEventListener('click', function () {
+document.getElementById('form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
   const searchrequest = document.getElementById('input').value.trim();
 
   if (!searchrequest) {
@@ -21,25 +23,24 @@ document.getElementById('searchbutton').addEventListener('click', function () {
 
   const loader = document.querySelector('.loader');
   loader.style.visibility = 'visible';
-  setTimeout(() => {
-    searchImages(searchrequest)
-      .then(function (images) {
-        displayImages(images);
-        new SimpleLightbox('.image-link', {
-          captionsData: 'alt',
-          captionDelay: 500,
-        }).refresh();
-      })
-      .catch(error => {
-        console.error('RENDER ERROR', error);
-        iziToast.error({
-          title: 'Error',
-          message: 'Failed to load images. Please try again!',
-          position: 'topRight',
-        });
-      })
-      .finally(() => {
-        loader.style.visibility = 'hidden';
+
+  searchImages(searchrequest)
+    .then(function (images) {
+      displayImages(images);
+      new SimpleLightbox('.image-link', {
+        captionsData: 'alt',
+        captionDelay: 500,
+      }).refresh();
+    })
+    .catch(error => {
+      console.error('RENDER ERROR', error);
+      iziToast.error({
+        title: 'Error',
+        message: 'Failed to load images. Please try again!',
+        position: 'topRight',
       });
-  }, 2000);
+    })
+    .finally(() => {
+      loader.style.visibility = 'hidden';
+    });
 });
